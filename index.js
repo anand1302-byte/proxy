d24a5a4ffe3234e0a7f608b0685b928b965257bf
Anand Jethava <anandjethava02@gmail.com>
2025-01-20 15:08:42 +0530

    commit

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express();
-app.use(cors());
+// Filter console warnings
+const originalConsoleWarn = console.warn;
+console.warn = (message) => {
+    if (!message.includes('specific warning keyword')) {
+            originalConsoleWarn(message);
+                }
+                };
 
-app.use('/proxy', createProxyMiddleware({
-    target: 'https://maps.googleapis.com',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/proxy': '', // Removes '/proxy' prefix
-    },
-}));
+                const app = express();
+                app.use(cors());
 
-app.listen(8080, () => console.log('CORS Proxy running on http://localhost:8080'));
+                app.use('/proxy', createProxyMiddleware({
+                    target: 'https://maps.googleapis.com',
+                        changeOrigin: true,
+                            pathRewrite: {
+                                    '^/proxy': '', // Removes '/proxy' prefix
+                                        },
+                                        }));
+
+                                        app.listen(8080, () => console.log('CORS Proxy running on http://localhost:8080'));
